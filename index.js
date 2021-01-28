ab179503bfe0bde733a2383c1d45d953213f4707
LivCodes <oliviareeddev@gmail.com>
2021-01-27 21:09:11 -0500

    color spectrum

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ console.log(square.style.left, square.style.top);
 document.addEventListener("keydown", function(e) {
   console.log(e.key)
   // left movement
-  if(e.key === 'ArrowLeft') {
+  if(e.key === 'ArrowLeft' && square.style.left !== `0px`) {
     let leftNum = square.style.left.replace('px', '')
   let newLeft = parseInt(leftNum, 10)
   square.style.left = `${newLeft - 10}px`
-  } else if (e.key === 'ArrowRight') { //right movement
+  } else if (e.key === 'ArrowRight' && square.style.left !== `300px`) { //right movement
     let rightNum = square.style.left.replace('px', '')
   let newRight = parseInt(rightNum, 10)
   square.style.left = `${newRight + 10}px`
-  } else if (e.key === 'ArrowUp') { //upward movement
+  } else if (e.key === 'ArrowUp' && square.style.top !== `0px`) { //upward movement
     let topNum = square.style.top.replace('px', '')
   let newTop = parseInt(topNum, 10)
   square.style.top = `${newTop - 10}px`
-  } else if (e.key === 'ArrowDown') {  //downward movement
+  } else if (e.key === 'ArrowDown' && square.style.top !== `300px`) {  //downward movement
     let downNum = square.style.top.replace('px', '')
   let newDown = parseInt(downNum, 10)
   square.style.top = `${newDown + 10}px`
@@ -25,3 +25,11 @@ document.addEventListener("keydown", function(e) {
 
 });
 
+document.addEventListener("click", function(a) {
+  if(a.target === square) {
+    let randomNum = Math.floor(Math.random() * 255)
+    let randomNum1 = Math.floor(Math.random() * 255)
+    let randomNum2 = Math.floor(Math.random() * 255)
+    square.style.backgroundColor = `rgb(${randomNum1}, ${randomNum2}, ${randomNum})`
+  }
+})
